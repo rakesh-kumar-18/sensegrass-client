d1804c964d33f383dddd288a79378d496b6721c7
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2025-01-14 11:48:06 +0530

    add sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+const Sidebar = ({ activeTab, setActiveTab }) => {
+    const tabs = [
+        { name: "Dashboard", key: "dashboard" },
+        { name: "Users", key: "users" },
+        { name: "Fields", key: "fields" },
+        { name: "Transactions", key: "transactions" },
+    ];
+
+    return (
+        <div className="w-64 bg-white shadow-md">
+            <div className="p-4 text-green-600 font-bold text-2xl">Agrifuture</div>
+            <ul className="space-y-4 p-4">
+                {tabs.map((tab) => (
+                    <li
+                        key={tab.key}
+                        onClick={() => setActiveTab(tab.key)}
+                        className={`cursor-pointer px-4 py-2 rounded-md ${activeTab === tab.key
+                            ? "bg-green-600 text-white"
+                            : "text-gray-600 hover:bg-gray-100"
+                            }`}
+                    >
+                        {tab.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
